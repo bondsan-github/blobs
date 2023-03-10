15daa8070d91e753dcfb85575d7e6badfbed905f
mark_t <markthompsonpersonal@gmail.com>
2023-03-10 11:48:44 +0000

    to animate

--- main.cs
+++ main.cs
@@ -14,83 +14,165 @@ namespace blobs
 {
     public class blob
     {
-        private float arc_amount = 100;
-        private float arc_length;
-        private List<PointF> verticies = new List<PointF>();
-        private float radius;
-        private Point center;
+        private int          quadrant_amount = 20;
+        
+        private List<PointF> vertices_start;
+        //public  List<PointF> Vertices_Start { get; set; }
+        
+        private List<PointF> vertices_end;
 
-        public blob( Point in_center , float in_radius )
+        private List<PointF> vertices_slope;
+
+        public  float        radius { get; }
+
+        private Point        center;
+        private float        tension = 1.0f;
+        private Pen          pen_blue;
+        private float        pen_width = 1.0f;
+        private Random       random;
+        private float        variance = 40.0f;
+
+        private float        slope_divisions = 60;
+
+        public blob( float in_radius )//, Point in_center )
         {
-            center = in_center;
-            radius = in_radius;
+            //center   = in_center;
+            radius   = in_radius;
+            random   = new Random();
+            pen_blue = new Pen( Color.Blue , pen_width );
 
-            float pi_double = 3.141592653F * 2F;
+            vertices_start = new List<PointF>();
+            vertices_end   = new List<PointF>();
+            vertices_slope = new List<PointF>();
+        }
 
-            float theta = pi_double / arc_amount;
-            arc_length = ( theta / pi_double ) * pi_double * radius;
+        public void set_center( Point in_center ) { center = in_center; }
 
-            for( double circumference = 0; circumference < 2 * Math.PI; circumference += arc_length)
+        public void initialise()
+        {
+            float pi_double = 3.141592653F * 2F;// full circle in radians
+
+            float theta = pi_double / quadrant_amount; // quadrant angle
+
+            for ( float circumference_point = 0; circumference_point <= pi_double + theta; circumference_point += theta )
             {
-                float x = center.X + radius * (float)Math.Cos(theta);
-                float y = center.Y + radius * (float)Math.Sin(theta);
+                float x = center.X + ( radius * (float)Math.Cos(circumference_point) );
+                float y = center.Y + ( radius * (float)Math.Sin(circumference_point) );
 
-                verticies.Add( new PointF( x, y ) );
+                PointF new_vertex_start = new PointF();
+                PointF new_vertex_end = new PointF();
+                
+                new_vertex_start.X = x += ( float )random.NextDouble() * variance;
+                new_vertex_start.Y = y += ( float )random.NextDouble() * variance;
+
+                new_vertex_end.X = x += (float)random.NextDouble() * variance;
+                new_vertex_end.Y = y += (float)random.NextDouble() * variance;
+
+                vertices_start.Add(new_vertex_start);
+                vertices_end.Add(new_vertex_end);
+            }
+
+            // last vertex = first vertex
+            vertices_start[ vertices_start.Count - 1 ] = vertices_start[ 0 ];
+            vertices_end  [ vertices_end.Count - 1   ] = vertices_end[ 0 ];
+
+            for ( int index = 0 ; index < vertices_start.Count ; index++ )
+            {
+                PointF new_point = new PointF();
+                
+                new_point.X = ( vertices_start[ index ].X - vertices_end[ index ].X ) / slope_divisions;
+                new_point.Y = ( vertices_start[ index ].Y - vertices_end[ index ].Y ) / slope_divisions;
+
+                vertices_slope.Add( new_point );
             }
         }
 
-        public void paint(object sender, PaintEventArgs event_paint)
+        public void paint( object sender, PaintEventArgs event_paint )
         {
-            //center.X = (size_client.Width / 2) - (int)radius / 2;
-            //center.Y = (size_client.Height / 2) - (int)radius / 2;
+            event_paint.Graphics.DrawCurve( pen_blue , vertices_start.ToArray() , tension );
+        }
 
-            Rectangle position = new Rectangle(center, new Size((int)radius, (int)radius));
+        public void update()
+        {
+            for ( int index = 0 ; index < vertices_start.Count ; index++ )
+            {
+                PointF new_point = vertices_start[index];
 
-            event_paint.Graphics.FillEllipse(Brushes.Blue, position);
+                new_point.X += vertices_slope[ index ].X;
+                new_point.Y += vertices_slope[ index ].Y;
+
+                vertices_start[ index ] = new_point;
+            }
+        }
+
+        public void recenter( PointF new_center )
+        {
+            //foreach ( var vertex in Vertices_Start )
         }
     }
 
-    class application : Form
+    class form : Form
     {
         Point form_center;
-        Timer timer_50ms;
+        Timer timer_1;
+        BufferedGraphicsContext graphics_buffered_context;
+        BufferedGraphics graphics_buffer;
 
+        float blob_radius = 150.0f;
         blob blob1;
 
-        public application()
+        public form()
         {
+            // form state
             this.WindowState = FormWindowState.Maximized;
-            this.KeyDown += application_KeyDown;
-            this.Paint += application_Paint;
-
-            timer_50ms = new Timer();
-            timer_50ms.Interval = 50;
-            timer_50ms.Tick += timer_50ms_tick;
-            timer_50ms.Start();
-            
             ResizeRedraw = true;
+            DoubleBuffered = true;
+            // event handlers
+            this.KeyDown += form_KeyDown;
+            this.Paint += form_Paint;
+            this.Load += form_Load;
+            //this.Resize += form_Resize;
+            // timer
+            timer_1 = new Timer();
+            timer_1.Interval = 16;
+            timer_1.Tick += timer_1_tick;
+            timer_1.Start();
+            // blob
+            
+            //form_center.X = ( ClientSize.Width / 2 ) - ( int )blob_radius / 2;
+            //form_center.Y = ( ClientSize.Height / 2 ) - ( int )blob_radius / 2;
 
-            float blob_size = 50F;
+            blob1 = new blob( blob_radius );//, form_center );
+            this.Paint += new System.Windows.Forms.PaintEventHandler( this.blob1.paint );
 
-            form_center.X = ( ClientSize.Width  / 2 ) - (int)blob_size / 2;
-            form_center.Y = ( ClientSize.Height / 2 ) - (int)blob_size / 2;
-
-            blob1 = new blob( form_center, blob_size );
-
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.blob1.paint );
+            // Gets a reference to the current BufferedGraphicsContext
+            graphics_buffered_context = BufferedGraphicsManager.Current;
+            // Creates a BufferedGraphics instance associated with Form1, and with
+            // dimensions the same size as the drawing surface of Form1.
+            graphics_buffer = graphics_buffered_context.Allocate( this.CreateGraphics() , this.DisplayRectangle );
         }
 
 
-        private void timer_50ms_tick(object sender, EventArgs e)
+        private void timer_1_tick(object sender, EventArgs e)
         {
+            blob1.update();
         }
 
-        private void application_Paint(object sender, PaintEventArgs e)
+        private void form_Paint(object sender, PaintEventArgs e)
         {
+           
         }
 
+        public void form_Load(object sender, EventArgs event_args)
+        {
+            form_center.X = ( ClientSize.Width / 2 ) - ( int )blob1.radius / 2;
+            form_center.Y = ( ClientSize.Height / 2 ) - ( int )blob1.radius / 2;
 
-        private void application_KeyDown( object sender, KeyEventArgs event_arguments ) 
+            blob1.set_center( form_center );
+            blob1.initialise();
+        }
+
+        private void form_KeyDown( object sender, KeyEventArgs event_arguments ) 
         {
             if( event_arguments.KeyCode == Keys.Escape )
             {
@@ -100,7 +182,7 @@ namespace blobs
 
         public static void Main( string[] args )
         {
-            Application.Run( new application() );
+            Application.Run( new form() );
         }
     }
 
